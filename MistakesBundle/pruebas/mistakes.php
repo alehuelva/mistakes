279c4b38fedc97bf52248efbc33d4d6b3334309f
alehuelva <alehuelva13@gmail.com>
2012-05-09 14:31:24 +0200

    cambios

--- MistakesBundle/pruebas/mistakes.php
+++ MistakesBundle/pruebas/mistakes.php
@@ -3,38 +3,41 @@
 
 class mistakes {
 	
-	public function getProjects() {
+	public $projects= array(); //CAMBIAR ESTO no son public
+	public $projectsname = array ();
+	
+	
+	public function loadProjects() { // Get projects from xml
 		$projectsfile =file_get_contents("http://kunstmaan.airbrake.io/data_api/v1/projects.xml?auth_token=5047b6b5e6910cafa77422f04d06ae2097bd05ff");
 		$xmlprojects = new SimpleXMLElement($projectsfile);
 		return $xmlprojects;
 	}
+
 	
-	
-	
-	public function getErrors($pageid=0) {
+	public function loadErrors($pageid=0) { //get errors from xml
 		$errorsfile =file_get_contents("http://kunstmaan.airbrake.io/errors.xml?auth_token=5047b6b5e6910cafa77422f04d06ae2097bd05ff&page=" . $pageid);
 		$xmlerrors = new SimpleXMLElement($errorsfile);
 		return $xmlerrors;
 	}
-}
+	
+	public function resetErrors($xmlprojects) { //Reset the accounting of errors in the array
+		if ($xmlprojects != NULL){
+			foreach ($xmlprojects->project as $proj) {
+				$projects[(int)$proj->id] = 0;
+				$projectsname[(int)$proj->id] = $proj -> name;
+			}
+		} else echo "Error loading projects";
+		
+		//return ?
+		}
 
-  public function count
-
- 
-$projects= array();
-$projectsname = array ();
-
- 
-foreach ($xmlprojects->project as $proj) {
-	$projects[(int)$proj->id] = 0;
-	$projectsname[(int)$proj->id] = $proj -> name;
-	//$projects[(int)$proj->id][$name] = (string)$proj->name;
-	 
-}
- 
-foreach ($xmlerrors->group as $group) {
-	$projects[(int)$group -> {'project-id'}]++;
-}var_dump ($projects);
+  	public function getErrors($xmlerrors) {
+			if ($xmlerrors != NULL){
+				foreach ($xmlerrors->group as $group) {
+				$projects[(int)$group -> {'project-id'}]++;
+				}
+			}else echo "Error al cargar proyectos";
+  	}
  
  
  
@@ -52,4 +55,6 @@ foreach($projects as $proj => $cont){
 	<td>' . $cont . '</td>
 
 	</tr>';
+}
+  }
 }
